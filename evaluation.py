c70ce3b5ebf731d8390d28d11832a4b12c0d8730
inesnolas <madzimia@gmail.com>
2024-02-15 15:25:15 +0000

    updated repo

--- evaluation.py
+++ evaluation.py
@@ -27,55 +27,30 @@ def discard_events_outside_experiment_window(exp_start, exp_end, gt_events, pred
       
     return  new_gt_events, new_predicted_events, new_predicted_events_frames
 
-def double_onset_correction(onsets_predicted, gt_onsets, correction= 0.020):
-    '''Correct double onsets by removing onsets which are less than a given threshold in time.
-    Args:
-        onsets_predicted (list): List of predicted onsets.
-        gt_onsets (list): List of ground truth onsets.
-        correction (float): Threshold in seconds.
-    Returns:
-        list: Corrected predicted onsets.
-    '''    
-    # Calculate interonsets difference
-    gt_onsets = np.array(gt_onsets, dtype=float)
 
-    # Calculate the difference between consecutive onsets
-    differences = np.diff(onsets_predicted)
-
-    # Create a list to add the filtered onset and add a first value
-
-    filtered_onsets = [onsets_predicted[0]]  #Add the first onset
-
-    # Subtract all the onsets which are less than fixed threshold in time
-    for i, diff in enumerate(differences):
-      if diff >= correction:
-      # keep the onset if the difference is more than the given selected time
-        filtered_onsets.append(onsets_predicted[i + 1])
-        #print the number of onsets predicted after correction
-    return filtered_onsets
-      
 #######################** ALGORITHMS**#############################################
-################################################################################### COMPARE WITH ABOVE??
+################################################################################### 
 #
 # #  Compute filtering of onset detection function
-def double_onsets_correction(onsets_predicted, gt_onsets, correction= 0.020):    
-    # Calculate interonsets difference
-    gt_onsets = np.array(gt_onsets, dtype=float)
+def double_onset_correction(onsets_predicted, correction=0.02):    
+    
+    if not correction == 0 or len(onsets_predicted) > 1:
+        # Calculate the difference between consecutive onsets
+        differences = np.diff(onsets_predicted)
 
-    # Calculate the difference between consecutive onsets
-    differences = np.diff(onsets_predicted)
+        # Create a list to add the filtered onset and add a first value
 
-    # Create a list to add the filtered onset and add a first value
+        filtered_onsets = [onsets_predicted[0]]  #Add the first onset
 
-    filtered_onsets = [onsets_predicted[0]]  #Add the first onset
-
-    # Subtract all the onsets which are less than fixed threshold in time
-    for i, diff in enumerate(differences):
-      if diff >= correction:
-      # keep the onset if the difference is more than the given selected time
-        filtered_onsets.append(onsets_predicted[i + 1])
-        #print the number of onsets predicted after correction
-    return filtered_onsets
+        # Subtract all the onsets which are less than fixed threshold in time
+        for i, diff in enumerate(differences):
+            if diff >= correction:
+            # keep the onset if the difference is more than the given selected time
+                filtered_onsets.append(onsets_predicted[i + 1])
+                #print the number of onsets predicted after correction
+        return np.asarray(filtered_onsets)
+    else:
+        return onsets_predicted
       
 ############################################################################################
 ############################################################################################
