57bc336589d756831906a562146e0027bd90f92d
antorr91 <antonella.m.c.torrisi@qmul.ac.uk>
2024-04-08 18:42:06 +0100

    'update'

--- run_testing_F0_pyin.py
+++ run_testing_F0_pyin.py
@@ -10,80 +10,185 @@ import basic_pitch as bp
 import time
 import matplotlib.pyplot as plt
 import librosa as lb
+import utils as ut
+import scipy.signal as signal
+import scipy.stats as stats
+import soundfile as sf
 
-# create dictionary with the duration of the calls for each chick
 
 # Path to the folder containing the txt files to be evaluated
-audio_folder = 'C:\\Users\\anton\\Chicks_Onset_Detection_project\Data\\normalised_data_only_inside_exp_window\\Testing_set'
+#audio_folder = 'C:\\Users\\anton\\Chicks_Onset_Detection_project\Data\\normalised_data_only_inside_exp_window\\Testing_set'
 
 #metadata = pd.read_csv("C:\\Users\\anton\\Data_normalised\\Testing_set\\chicks_testing_metadata.csv")
-# save_results_folder = r'C:\\Users\\anton\\Chicks_Onset_Detection_project\\Segmentation_task\\testing_calls'
-# if not os.path.exists(save_results_folder):
-#     os.makedirs(save_results_folder)
 
-# list_files = glob.glob(os.path.join(audio_folder, "*.wav"))
-file= 'C:\\Users\\anton\\Chicks_Onset_Detection_project\\Data\\normalised_data_only_inside_exp_window\\Testing_set\\chick367_d1.wav'
 
-# for file in tqdm(list_files):
+audio_folder = 'C:\\Users\\anton\\Chicks_Onset_Detection_project\\Subset_features'
+
+list_files = glob.glob(os.path.join(audio_folder, "*.wav"))
+
+#file= 'C:\\Users\\anton\\Chicks_Onset_Detection_project\\Data\\normalised_data_only_inside_exp_window\\Testing_set\\chick367_d1.wav'
+
+
+
+#metadata = pd.read_csv("C:\\Users\\anton\\Data_normalised\\Testing_set\\chicks_testing_metadata.csv")
+save_results_folder = r'C:\\Users\\anton\\Chicks_Onset_Detection_project\\Segmentation_task\\subset_calls\\new_test'
+if not os.path.exists(save_results_folder):
+    os.makedirs(save_results_folder)
+
+
+# for i in range(len(parameters)):
+    # Iterate over each audio file
+for file in tqdm(list_files):
+    
+    chick = os.path.basename(file)[:-4]
+
+    ##### 1- Load audio file
+    audio_y, sr = lb.load(file, sr=44100)
+
+
+    ##### 2- Apply the Bandpass filter
+    # The Bandpass filter is applied to the audio signal to remove 
+    # the background noise and keep only the chick's vocalizations ( among the frequencies 2000-12500 Hz)
+
+    audio_y = ut.bp_filter(audio_y, sr, lowcut=1800, highcut=12500)
+
+
+    # Parameters for spectrogram and pitch estimation
+    frame_length = 2048
+    hop_length = 512
+    win_length = frame_length // 2
+
+    # Parameters for PYIN
+    threshold = 100
+    beta_parameters = (0.10, 0.10)
+    fmin = 1800
+    fmax = 12500
+    resolution = 0.01   
+
+    # # # Estimate pitch using PYIN    
+    f0_pyin_lb, voiced_flag, voiced_probs = lb.pyin(audio_y, sr=sr, frame_length=frame_length, hop_length=hop_length, 
+    fmin=fmin, fmax=fmax, n_thresholds=threshold, beta_parameters=beta_parameters, resolution=resolution)
+    
+    f0_in_times = lb.times_like(f0_pyin_lb, sr=sr, hop_length=hop_length)
+    #save f0_pyin_lb to csv
+    f0_pyin_lb_df = pd.DataFrame(f0_pyin_lb)
+    # replace nan in dataframe with 0
+    f0_pyin_lb_df = f0_pyin_lb_df.fillna(0)
+    # save f0_pyin_lb to csv in frame 
+    # Save pitch data to CSV
+    f0_pyin_df = pd.DataFrame(f0_in_times, columns=['F0_in_Time'])
+    f0_pyin_df.to_csv(chick + '_f0_pyin_.csv', index=False)
+
+
    
-chick = os.path.basename(file)[:-4]
+    ########################################################################################################################################
 
-# Load audio file
-#file = lb.ex("humpback")
-audio_y, sr = lb.load(file, sr=22050,duration=20.0)
-# duration = lb.get_duration(y=audio_y, sr=sr)
-duration = 20.0
+    # # Compute the first derivative of the frequency
+    # f0_pyin_diff = np.diff(f0_pyin_lb)
 
+    # # Compute the times for the first derivative of the frequency
+    # f0_pyin_diff_times = f0_in_times[:-1]
 
-# Parameters for spectrogram and pitch estimation
-frame_length = 2048
-hop_length = 512
-n_fft = frame_length * 2
-win_length = frame_length // 2
-# in sonic visualiser audio frame per block is 2048, window lenght is 2
+    # # Plot the first derivative of the frequency
+    # plt.figure(figsize=(10, 5))
+    # plt.plot(f0_pyin_diff_times, f0_pyin_diff, label='First Derivative of Frequency', color='green')
 
-st = time.time()
-f0_pyin_lb, voiced_flag, voiced_probs = lb.pyin(audio_y, sr=sr, frame_length=frame_length, hop_length=hop_length, fmin=1000, fmax=10000)
-et= time.time()
-print('it takes ' + str(et-st)+ ' seconds to run pyin for a file of duration ' + str(duration) + ' seconds')
-f0_in_times= lb.times_like(f0_pyin_lb, sr=sr, hop_length=hop_length)
-# save f0_pyin_lb to csv
-f0_pyin_lb_df = pd.DataFrame(f0_pyin_lb)
-# replace nan in dataframe with 0
-f0_pyin_lb_df = f0_pyin_lb_df.fillna(0)
+   
+    ########################################################################################################################################
+    # Compute the spectral centroid and then extract the mean
+    #spectral_centroid = lb.feature.spectral_centroid(y=audio_y, sr=sr, n_fft=frame_length, hop_length=hop_length)
 
-f0_pyin_lb_df.to_csv(chick + '_f0_pyin_lb.csv', index=False)
-# plot spectrogram with f0_in_times
-np.save( chick + '_f0_pyin_lb.npy', f0_pyin_lb)
-# print('f0_pyin_lb, voiced_
-print('f0_pyin_lb', f0_pyin_lb)
-
-# Plotting
-slice_st_sec =20
-slice_st_frame = int(slice_st_sec*sr/hop_length)
-
-slice_et_sec = 30
-slice_et_frame = int(slice_et_sec*sr/hop_length)
+    # compute the times for the spectral centroid
+    #spectral_centroid_times = lb.times_like(spectral_centroid[0], sr=sr, hop_length=hop_length)
 
 
 
-# Compute melspectrogram (makes it easier to see pitch output)
-S = lb.feature.melspectrogram(y=audio_y, sr=sr, win_length=win_length, hop_length=hop_length, n_mels=128)
-S_db = lb.amplitude_to_db(S, ref=np.max)
+    #######################################################################################################################################
+    # # Compute the spectral bandwidth and then extract the mean
+    # lin_spec= np.abs(lb.stft(y=audio_y, n_fft=frame_length, hop_length=hop_length))
+    # # Compute the frequencies
+    # frequencies = lb.fft_frequencies(sr=sr, n_fft=frame_length)
+
+    # energy_y = np.array([sum(abs(audio_y[i:i+frame_length]**2))for i in range(0, len(audio_y), hop_length)])
 
 
-f0_slice = f0_pyin_lb[slice_st_frame:slice_et_frame + 1]
-f0_slice_times = f0_in_times[slice_st_frame:slice_et_frame + 1]
 
-plt.figure(figsize=(10, 6))
-# Plot pitch
-plt.plot(f0_in_times, f0_pyin_lb, label='Pitch (Hz)', color='red')
-# Plot Mel spectrogram
-lb.display.specshow(S_db, sr=sr, x_axis='time', y_axis='mel', cmap='viridis', alpha=0.75)
-plt.colorbar(format='%+2.0f dB')
-plt.title('Pitch and Mel Spectrogram')
-plt.xlabel('Time (s)')
-plt.ylabel('Frequency')
-plt.legend()
-plt.tight_layout()
-plt.show()
+    # # Apply the mask to the frequencies CREATE A  FUNCTION MASK FOR THE FREQUENCIES
+    # low_mask = np.logical_and(frequencies >= 0, frequencies <= 2000)
+    # lin_spec[low_mask] = 0
+    # # Apply second mask over the frequencies between 8000 and 11025
+    # high_mask = np.logical_and(frequencies >= 12500, frequencies <= 20000)
+    # lin_spec[high_mask] = 0
+
+
+    # # Compute the rms (loudness) of the audio
+    # rms_y = lb.feature.rms(S=lin_spec, frame_length=frame_length, hop_length=hop_length)
+    # # compute the times for the rm
+    # audio_duration = len(audio_y) / sr
+
+    # rms_y_times = lb.frames_to_time(np.arange(rms_y.shape[-1]), sr=sr, hop_length=hop_length)
+    # # create a dataframe with the rms values
+    # rms_y_df = pd.DataFrame(rms_y_times, columns=['RMS'])
+    # # save rms_y to csv
+    # rms_y_df.to_csv(chick + '_rms_.csv', index=False)
+    # # Plot the linear spectrogram
+    # plt.figure(figsize=(30, 8))
+    # lb.display.specshow(lb.amplitude_to_db(lin_spec, ref=np.max), sr=sr, hop_length=hop_length, x_axis='time', y_axis='linear', cmap='viridis', alpha=0.75)
+
+    # # Transpose if the matrix is not in the right shape
+    # rms_y_transposed = rms_y.T
+    # # Plot the RMS
+    # plt.plot(rms_y_times, rms_y_transposed, label='RMS', color='white')
+    # plt.colorbar(format='%+2.0f dB')
+    # plt.title('Linear Spectrogram and RMS')
+    # plt.xlabel('Time (s)')
+    # plt.ylabel('Frequency')
+    # plt.legend()
+    # plt.tight_layout()
+
+
+    ########################################################################################################################################
+    # plt.tight_layout()
+    # Spec, phase = lb.magphase(lb.stft(y=audio_y))
+    # rms_y= lb.feature.rms(y= Spec, frame_length=frame_length, hop_length=hop_length)
+    # rms_y_times = lb.times_like(rms_y, sr=sr, hop_length=hop_length) 
+    # # # save rms_y to csv
+    # # rms_y_df = pd.DataFrame(rms_y)
+    # # # replace nan in dataframe with 0
+    # # rms_y_df = rms_y_df.fillna(0)
+    # # # save rms_y to csv in frame
+    # # # Save pitch data to CSV
+    # # rms_y_df = pd.DataFrame(rms_y, columns=['RMS'])
+    # # rms_y_df.to_csv(chick + '_rms_.csv', index=False)
+
+
+    # # Compute Mel spectrogram
+    S = lb.feature.melspectrogram(y=audio_y, sr=sr, win_length=win_length, hop_length=hop_length, n_mels=128)
+    S_db = lb.amplitude_to_db(S, ref=np.max)
+    # Plot Mel spectrogram
+    plt.figure(figsize=(30, 8))
+    lb.display.specshow(S_db, sr=sr, x_axis='time', y_axis='mel', cmap='viridis', alpha=0.75)
+    
+    # Plot pitch (F0)
+    #plt.plot(f0_in_times, f0_pyin_lb, label='Pitch (Hz)', color='red')
+
+    # # Plot the RMS
+    # plt.plot(rms_y_times, rms_y_transposed, label='RMS', color='white')
+
+    # Plot the spectral centroid
+    #plt.plot(spectral_centroid_times[0], spectral_centroid[0], label='Spectral Centroid', color='green')
+    #plt.plot(spectral_centroid_times, spectral_centroid[0], label='Spectral Centroid', color='green')
+
+
+    plt.colorbar(format='%+2.0f dB')
+    plt.title('Spectral Centroid in Mel-spectrogram')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency')
+    plt.legend()
+    plt.tight_layout()
+    
+    # # Save the plot with the chick's name
+    plt.savefig(os.path.join(save_results_folder, f"{chick}_F0_thr_{threshold}_beta_{beta_parameters}_fmin_{fmin}_fmax_{fmax}_resolution_{resolution}.png"))
+    
+    # Show the plot
+    #plt.show()
--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ import pandas as pd
 import librosa as lb
 import matplotlib.pyplot as plt
 import soundfile as sf
+import scipy.signal as signal
+
+
 
 #This  function was built to calculate the duration of the calls from the ground truth onsets and offsets
 def calculate_durations(events):   
@@ -16,12 +19,23 @@ def calculate_durations(events):
 
 
 
+def bp_filter(audio_y, sr=44100, lowcut=2050, highcut=8000):
+    # Apply the bandpass filter
+    nyquist = 0.5 * sr
+    low = lowcut / nyquist
+    high = highcut / nyquist
+    b, a = signal.butter(5, [low, high], btype='band')
+    audio_y = signal.filtfilt(b, a, audio_y)
+
+    return audio_y
+
+
 
 
 
 def save_spec_segments(segments, filenames):
     for segment, filename in zip(segments, filenames):
-        plt.figure(figsize=(6, 4))
+        plt.figure(figsize=(4, 2))
         plt.imshow(segment, aspect='auto', origin='lower')
         plt.axis('off')
         plt.savefig(filename, bbox_inches='tight', pad_inches=0)
@@ -31,26 +45,29 @@ def save_spec_segments(segments, filenames):
 
 
 # This function was built to segment the calls using onsets and offsets and saving the spectrogram segments
+def get_calls_spec( audio_y , onsets, offsets, save_folder, sr=44100, hop_length=512, n_fft=2048*2):
 
-def get_calls_spec(filename, onsets, offsets, save_folder):
-    # Load the audio file
-    y, sr = lb.load(filename, sr=44100)
+    # compute the duration of the audio file
+    duration= lb.get_duration(audio_y, sr=sr)
 
     # Initialize lists to store spectrogram slices and their corresponding filenames
-    calls = []
+    calls_S = []
     call_filenames = []
 
     # Loop through each onset and offset pair
     for i, (onset, offset) in enumerate(zip(onsets, offsets)):
         # Convert time (in seconds) to sample indices
-        onset_frame = lb.time_to_samples(onset, sr=44100)
-        offset_frame = lb.time_to_samples(offset, sr=44100)
+        onset_sample = lb.time_to_samples(onset, sr=sr)
+        offset_sample = lb.time_to_samples(offset, sr=sr)
 
-        # Extract the spectrogram slice from onset to offset
-        call_audio = y[onset_frame: offset_frame]
-        
+        #Extract the spectrogram slice from onset to offset
+        epsilon = duration*0.01
+        epsilon_samples = lb.time_to_samples(epsilon, sr=44100)
+
+        call_audio = audio_y[onset_sample: offset_sample + epsilon_samples]
+
         # Compute the linear spectrogram of the call
-        D = np.abs(lb.stft(call_audio, n_fft=2048, hop_length=512))
+        D = np.abs(lb.stft(call_audio, n_fft=n_fft, hop_length=hop_length))
 
         # Apply logarithmic transformation to the spectrogram
         log_D = lb.amplitude_to_db(D, ref=np.max)
@@ -61,20 +78,20 @@ def get_calls_spec(filename, onsets, offsets, save_folder):
         scaled_log_D = (log_D - min_db) / (max_db - min_db)
 
         # Append the scaled log-spectrogram slice to the calls list
-        calls.append(scaled_log_D)
+        calls_S.append(scaled_log_D)
 
-        # Define filename for saving the spectrogram segment
-        call_filename = os.path.join(save_folder, f"segment_{i}.png")
-        call_filenames.append(call_filename)
+        # # Define filename for saving the spectrogram segment
+        # call_filename = os.path.join(save_folder, f"segment_{i}.png")
+        # call_filenames.append(call_filename)
 
-        # Save the spectrogram segment
-        plt.figure(figsize=(6, 4))
-        plt.imshow(scaled_log_D, aspect='auto', origin='lower')
-        plt.axis('off')
-        plt.savefig(call_filename, bbox_inches='tight', pad_inches=0)
-        plt.close()
-
-    return calls, call_filenames
+        # # Save the spectrogram segment
+        # plt.figure(figsize=(35, 7))
+        # plt.imshow(scaled_log_D, aspect='auto', origin='lower')
+        # plt.axis('off')
+        # plt.savefig(call_filename, bbox_inches='tight', pad_inches=0)
+        # plt.close()
+        #save_spec_segments(calls, call_filenames)
+    return calls_S
 
 
 
@@ -82,6 +99,7 @@ def save_waveform_segments(segments, filenames):
     for segment, filename in zip(segments, filenames):
         #save the waveform segment to a .wav file
         sf.write(filename, segment, 44100)
+        
 
 def get_calls_waveform(filename, onsets, offsets, save_results_folder):
     # Load the audio file
@@ -111,4 +129,68 @@ def get_calls_waveform(filename, onsets, offsets, save_results_folder):
     save_waveform_segments(calls, segment_filenames)
 
     return calls
+
+
+
+
+
+def save_F0_segments(segments, filenames):
+    for segment, filename in zip(segments, filenames):
+        #save a csv file with the F0 values
+        segment.to_csv(filename, index=False)
+
+
+  
+
+
+# def get_calls_F0(F0_in_times, F0_in_frames, onsets, offsets):
+#     # Load the audio file
+
+#     sr= 22050
+#     # Initialize an empty list to store waveform segments and segment filenames
+#     calls = []
+#     segment_filenames = []
+
+#     # Loop through each onset and offset pair
+#     for i, (onset, offset) in enumerate(zip(onsets, offsets)):
+#         # Convert time (in seconds) to sample indices
+#         # onset_frame = lb.time_to_frames(onset, sr=sr)
+#         # offset_frame = lb.time_to_frames(offset, sr=sr)
+
+#         # Extract the waveform segment from onset to offset
+#         call_F0 = F0_in_times[onset:offset]
+
+#         # Append the waveform segment to the calls list
+#         calls.append(call_F0)
+
+#         # # Define filename for saving the waveform segment
+#         # segment_filename = os.path.join(save_results_folder, f"{os.path.basename(filename)}_segment_{i}.wav")
+#         # segment_filenames.append(segment_filename)
+
+#     # Save segments
+#     #save_F0_segments(calls, segment_filenames)
+
+#     return calls
+        
+
+def get_calls_F0(F0_in_times, F0_in_frames, onsets, offsets):
+    # Initialize an empty list to store F0 slices
+    calls = []
+
+    # Loop through each onset and offset pair
+    for onset, offset in zip(onsets, offsets):
+        # Find the indices corresponding to the onset and offset times
+        onset_index = lb.time_to_frames(onset, sr=22050)
+        offset_index = lb.time_to_frames(offset, sr=22050)
+
+        # Extract the F0 values within the specified time window
+        call_F0 = F0_in_frames[(F0_in_times >= onset) & (F0_in_times <= offset)]
+
+        # Append the F0 slice to the calls list
+        calls.append(call_F0)
+
+    return calls
+
+
+
 
